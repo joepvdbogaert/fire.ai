c710447294feba463e6fbe79c58a9ea817e0db09
Bogaert <j.v.d.bogaert@tue.nl>
2019-04-08 16:14:07 +0200

    Allow for retrieval and storage of batches in ReplayBuffer

--- work/spyro/memory.py
+++ work/spyro/memory.py
@@ -20,7 +20,7 @@ class BaseMemory(object):
 
     def _get_batch(self, indices):
         """Get data by indices."""
-        return tuple([operator.itemgetter(*indices)(self.memory[key]) for key in self.keys])
+        return tuple([np.vstack(operator.itemgetter(*indices)(self.memory[key])) for key in self.keys])
 
     def _get_single(self, index):
         """Get single item by index."""
@@ -36,10 +36,13 @@ class BaseMemory(object):
     def store(self, *args):
         """Store an experience tuple in memory. Take care that arguments are provided in the
         same order as the :code:`keys` argument when initializing.
-        """
-        assert len(self.keys) == len(args)
+        """ 
         for i, key in enumerate(self.keys):
             self.memory[key].append(args[i])
+
+    def store_batch(self, *args):
+        for i, key in enumerate(self.keys):
+            self.memory[key].extend(list(args[i]))
 
     @abstractmethod
     def sample(self, batch_size):
@@ -49,9 +52,9 @@ class BaseMemory(object):
 
 class ReplayBuffer(BaseMemory):
 
-    def __init__(self, size=50000, keys=["states", "actions", "rewards", "next_states"]):
+    def __init__(self, size=50000, keys=["states", "actions", "rewards", "next_states", "terminal"]):
         super().__init__(size=size, keys=keys)
 
     def sample(self, batch_size):
-        index = np.random.randint(len(self), size=batch_size)
+        index = np.random.randint(0, self.__len__(), size=batch_size)
         return self.get(index)
