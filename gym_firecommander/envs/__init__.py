ec14baae22cae14c1c84f0c67ed33495700557bc
Bogaert <j.v.d.bogaert@tue.nl>
2019-05-28 15:34:12 +0200

    Add FireCommanderV2TestEnv to init

--- gym_firecommander/envs/__init__.py
+++ gym_firecommander/envs/__init__.py
@@ -1,4 +1,6 @@
 from gym_firecommander.envs.firecommander_env import FireCommanderEnv
 from gym_firecommander.envs.firecommander_big_env import FireCommanderBigEnv
 from gym_firecommander.envs.firecommander_env_v2 import FireCommanderV2
-from gym_firecommander.envs.firecommander_test_envs import FireCommanderTestEnv, FireCommanderBigTestEnv
+from gym_firecommander.envs.firecommander_test_envs import (
+    FireCommanderTestEnv, FireCommanderBigTestEnv, FireCommanderV2TestEnv
+)
